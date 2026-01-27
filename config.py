a01f191b7ee9a8cb7b7f8dd7f7e0b783d260ca9f
Julia Parkhomenko <julia@parkhomenko.ru>
2026-01-27 20:43:54 +0300

    Feature: Deep linking for linear user paths

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHANNEL_ID = os.getenv("CHANNEL_ID")
+ADMIN_GROUP_ID = os.getenv("ADMIN_GROUP_ID")
+
+CHANNEL_NAME = "@kapremont_channel"
+CHANNEL_LINK = "https://t.me/kapremont_channel"
