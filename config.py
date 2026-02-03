e4d0589a4c38fbee7ddfb67dd330e17a40349809
Julia Parkhomenko <julia@parkhomenko.ru>
2026-02-03 10:50:26 +0300

    Fix: added quiz handler to main.py

--- config.py
+++ config.py
@@ -4,8 +4,23 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+CONTENT_BOT_TOKEN = os.getenv("CONTENT_BOT_TOKEN")
+VK_TOKEN = os.getenv("VK_TOKEN")
+VK_GROUP_ID = os.getenv("VK_GROUP_ID")
+LEADS_GROUP_CHAT_ID = int(os.getenv("LEADS_GROUP_CHAT_ID", "-1003370698977"))
 CHANNEL_ID = os.getenv("CHANNEL_ID")
-ADMIN_GROUP_ID = os.getenv("ADMIN_GROUP_ID")
+NOTIFICATIONS_CHANNEL_ID = os.getenv("NOTIFICATIONS_CHANNEL_ID")
+THREAD_ID_KVARTIRY = int(os.getenv("THREAD_ID_KVARTIRY", "2"))
+THREAD_ID_KOMMERCIA = int(os.getenv("THREAD_ID_KOMMERCIA", "5"))
+THREAD_ID_DOMA = int(os.getenv("THREAD_ID_DOMA", "8"))
+YANDEX_API_KEY = os.getenv("YANDEX_API_KEY")
+FOLDER_ID = os.getenv("FOLDER_ID")
+OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
+DATABASE_URL = os.getenv("DATABASE_URL")
+UPLOAD_PLANS_DIR = os.getenv("UPLOAD_PLANS_DIR")
+UPLOAD_DIR = os.getenv("UPLOAD_DIR")
+ADMIN_ID = int(os.getenv("ADMIN_ID", "223465437"))
+MINI_APP_URL = os.getenv("MINI_APP_URL")
 
 CHANNEL_NAME = "@kapremont_channel"
 CHANNEL_LINK = "https://t.me/kapremont_channel"
