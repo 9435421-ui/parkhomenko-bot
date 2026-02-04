b0a92538798017425792048ed021cc29133d7d07
Julia Parkhomenko <julia@parkhomenko.ru>
2026-02-04 20:58:44 +0300

    Исправлен импорт в handlers/__init__.py: from .quiz import quiz_router as router

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -2,8 +2,8 @@
 Обработчики команд и сообщений
 """
 from .start import router as start_router
-from .quiz import router as router
+from .quiz import quiz_router as router
 from .dialog import router as dialog_router
 from .invest import router as invest_router
 
-__all__ = ['start_router', 'router', 'dialog_router', 'invest_router']
+__all__ = ['start_router', 'quiz_router', 'dialog_router', 'invest_router']
