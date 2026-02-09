a1952046d70c6f99dd6b942459b694ecc66f8b53
Julia Parkhomenko <julia@parkhomenko.ru>
2026-02-09 03:59:20 +0300

    fix: удалить несуществующие импорты (zen_service, post_to_vk)

--- services/__init__.py
+++ services/__init__.py
@@ -2,13 +2,11 @@
 Сервисы бота
 """
 from .lead_service import lead_service, LeadService
-from .vk_service import vk_service, VKService, post_to_vk, vk_listener_loop
-from .zen_service import zen_service, ZenService, post_to_zen
+from .vk_service import vk_service, VKService
 from .competitor_spy import competitor_spy, CompetitorSpy, run_competitor_spy, run_geo_spy
 
 __all__ = [
     'lead_service', 'LeadService',
-    'vk_service', 'VKService', 'post_to_vk', 'vk_listener_loop',
-    'zen_service', 'ZenService', 'post_to_zen',
+    'vk_service', 'VKService',
     'competitor_spy', 'CompetitorSpy', 'run_competitor_spy', 'run_geo_spy'
 ]
