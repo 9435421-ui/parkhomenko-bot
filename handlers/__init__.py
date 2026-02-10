6d4249e8972bd1e2a3885db509e88e156d9079ee
Julia Parkhomenko <julia@parkhomenko.ru>
2026-02-10 12:35:57 +0300

    fix: admin_router import fixed

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -5,6 +5,6 @@ from .start import router as start_router
 from .quiz import router as quiz_router
 from .dialog import router as dialog_router
 from .content import content_router
-from .admin import router as admin_router
+from .admin import admin_router
 
 __all__ = ['start_router', 'quiz_router', 'dialog_router', 'content_router', 'admin_router']
