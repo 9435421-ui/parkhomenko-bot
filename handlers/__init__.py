2222d5442eaa9b8049f162df82244bfd9c8533d1
Julia Parkhomenko <julia@parkhomenko.ru>
2026-02-14 00:49:28 +0300

    fix: admin router теперь подключается первым

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -5,7 +5,7 @@ from .start import router as start_router
 from .quiz import router as quiz_router
 from .dialog import router as dialog_router
 from .content import content_router
-from .admin import admin_router
+from .admin import router as admin_router
 from .vk_publisher import VKPublisher
 from .max_uploader import MaxUploader
 
